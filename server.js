0d7f6e8371d517f2dda31e6d047e52abe057f3fe
Gonzalo <arecogonzalo@gmail.com>
2019-03-20 09:42:34 +0800

    finished

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-'use strict';
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
 
-var express = require('express');
-var cors = require('cors');
+const upload = multer({ dest: "uploads/" });
 
-// require and use "multer"...
-
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", (req, res) => {
+  res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
+app.get("/hello", (req, res) => {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Node.js listening ...");
 });
